24d1397182f6bb5e3a2e903b6199d3a56484edea
ManuelBlanc <manuel.blanc@estudiante.uam.es>
2019-06-17 11:49:43 +0200

    Locals for efficiency; comment changes

--- bit64.lua
+++ bit64.lua
@@ -8,10 +8,14 @@
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --]]
 
+-- ==============================================================================================
+-- Imports
+-- ==============================================================================================
+
 local bit = require("bit")
-local uint64_t = require("ffi").typeof("uint64_t")
--- If the FFI lib isn't available, try this instead:
---local uint64_t = function(x) return (x + 0LL) end
+local band, bor, bxor, bnot = bit.band, bit.bor, bit.bxor, bit.bnot
+local lshift, rshift, arshift = bit.lshift, bit.rshift, bit.arshift
+local bswap, tohex = bit.bswap, bit.tohex
 
 -- ==============================================================================================
 -- Constructor and destructuring
@@ -20,14 +24,14 @@ local uint64_t = require("ffi").typeof("uint64_t")
 -- We must clear the sign because the BitOp library uses signed 32-bit integers.
 -- We do so by clearing the sign bit with a logical right shift, and multiplying by 2.
 local function U64_join(hi, lo)
-    local rshift, band = bit.rshift, bit.band
+    local rshift, band = rshift, band
     hi = rshift(hi, 1) * 2 + band(hi, 1)
     lo = rshift(lo, 1) * 2 + band(lo, 1)
-    return uint64_t(hi)*0x100000000 + (lo % 0x100000000)
+    return (hi * 0x100000000ull) + (lo % 0x100000000)
 end
 
 local function U64_split(x)
-    return tonumber(x / 0x100000000), tonumber(x % 0x100000000)
+    return tonumber(x / 0x100000000),  tonumber(x % 0x100000000)
 end
 
 -- ==============================================================================================
@@ -48,59 +52,63 @@ end
 
 -- tohex checks the # of args, so we must use a vararg for the optional arg.
 local function U64_tohex(x, ...)
-    local tohex = bit.tohex
     local hi, lo = U64_split(x)
     return tohex(hi, ...) .. tohex(lo, ...)
 end
 
 -- ==============================================================================================
--- Basic bitwise operators
+-- Binary operations
 -- ==============================================================================================
 
--- Utility functions that map 64-bit int args to their hi/lo part.
-local function _U64_map_lo(x, ...)
-    if x then
-        return U64_lo(x), _U64_map_lo(...)
+-- Binary operation implementation
+local function _binop(op, x1, x2, ...)
+    local hi1, lo1 = U64_split(x1)
+    local hi2, lo2 = U64_split(x2)
+    local hi, lo = op(hi1, hi2), op(lo1, lo2)
+    for n=1, select("#", ...) do
+        local hi_n, lo_n = U64_split(select(n, ...))
+        hi = op(hi, hi_n)
+        lo = op(lo, lo_n)
     end
-end
-local function _U64_map_hi(x, ...)
-    if x then
-        return U64_hi(x), _U64_map_hi(...)
-    end
+    return U64_join(hi, lo)
 end
 
 local function U64_band(...)
-    local band = bit.band
-    return U64_join(band(_U64_map_hi(...)), band(_U64_map_lo(...)))
+    return _binop(band, ...)
 end
 
 local function U64_bor(...)
-    local bor = bit.bor
-    return U64_join(bor(_U64_map_hi(...)), bor(_U64_map_lo(...)))
+    return _binop(bor, ...)
 end
 
 local function U64_bxor(...)
-    local bxor = bit.bxor
-    return U64_join(bxor(_U64_map_hi(...)), bxor(_U64_map_lo(...)))
+    return _binop(bxor, ...)
 end
+
+-- ==============================================================================================
+-- Unary operations
+-- ==============================================================================================
 
 local function U64_bnot(x)
     local hi, lo = U64_split(x)
-    local bnot = bit.bnot
+    local bnot = bnot
     return U64_join(bnot(hi), bnot(lo))
 end
 
+local function U64_bswap(x)
+    local hi, lo = U64_split(x)
+    return U64_join(bswap(lo), bswap(hi)) -- lo and hi are swapped
+end
+
 -- ==============================================================================================
--- Bit shifting operators
+-- Shifting operations
 -- ==============================================================================================
 
 local function U64_lshift(x, n)
-    local band = bit.band
     if band(n, 0x3F) == 0 then return x end
     local hi, lo = U64_split(x)
-    local lshift = bit.lshift
     if band(n, 0x20) == 0 then
-        lo, hi = lshift(lo, n), bit.bor(lshift(hi, n), bit.rshift(lo, 32 - n))
+        lo, hi = lshift(lo, n), bor(lshift(hi, n), rshift(lo, 32 - n))
     else
         lo, hi = 0, lshift(lo, n)
     end
@@ -108,12 +116,10 @@ local function U64_lshift(x, n)
 end
 
 local function U64_rshift(x, n)
-    local band = bit.band
     if band(n, 0x3F) == 0 then return x end
     local hi, lo = U64_split(x)
-    local rshift = bit.rshift
     if band(n, 0x20) == 0 then
-        lo, hi = bit.bor(rshift(lo, n), bit.lshift(hi, 32 - n)), rshift(hi, n)
+        lo, hi = bor(rshift(lo, n), lshift(hi, 32 - n)), rshift(hi, n)
     else
         lo, hi = rshift(hi, n - 32), 0
     end
@@ -121,31 +127,23 @@ local function U64_rshift(x, n)
 end
 
 local function U64_arshift(x, n)
-    local band = bit.band
     if band(n, 0x3F) == 0 then return x end
     local hi, lo = U64_split(x)
-    local arshift = bit.arshift
     if band(n, 0x20) == 0 then
-        lo, hi = bit.bor(bit.rshift(lo, n), bit.lshift(hi, 32 - n)), arshift(hi, n)
+        lo, hi = bor(rshift(lo, n), lshift(hi, 32 - n)), arshift(hi, n)
     else
         lo, hi = arshift(hi, n - 32), arshift(hi, 31)
     end
     return U64_join(hi, lo)
 end
 
--- ==============================================================================================
--- Bit shifting operators
--- ==============================================================================================
-
 local function U64_rol(x, n)
-    local band = bit.band
     if band(n, 0x3F) == 0 then return x end
     local hi, lo = U64_split(x)
     if band(n, 0x20) ~= 0 then
         lo, hi = hi, lo
     end
     if band(n, 0x1F) ~= 0 then
-        local bor, lshift, rshift = bit.bor, bit.lshift, bit.rshift
         lo, hi = bor(lshift(lo, n), rshift(hi, 32 - n)), bor(lshift(hi, n), rshift(lo, 32 - n))
     end
     return U64_join(hi, lo)
@@ -155,12 +153,6 @@ end
 
 local function U64_ror(x, n)
     return U64_rol(x, 64 - n)
-end
-
-local function U64_bswap(x)
-    local hi, lo = U64_split(x)
-    local bswap = bit.bswap
-    return U64_join(bswap(lo), bswap(hi)) -- lo and hi are swapped
 end
 
 -- ==============================================================================================
@@ -196,5 +188,6 @@ setmetatable(bit64, {
         )
     end,
 })
+
 
 return bit64
